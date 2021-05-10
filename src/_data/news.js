3e763a89c998771038c87563e43b7a8c02eaa43f
NinoJongmans <ninojongmans@gmail.com>
2021-05-10 14:26:30 +0200

    1

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=nl&apiKey=${process.env.API_KEY}&pageSize=5`);
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=nl&apiKey=ebfa3f65c3554ecfbbba3630a823faa3&pageSize=5`);
     return response.data;
   } catch (error) {
     console.error(error);
